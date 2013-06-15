1ba81b8b226c962b6ca2c823083865667b0713fc
Justin Bachorik <justinbach@gmail.com>
2013-06-15 15:40:45 -0400

    created Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoveItWeb::Application.config.secret_token = '267906f88e3b71ed21a649e98dfeadf06cd1caffe9b407a459eaa65359e269b0f487a4578479a8d81923c8ec3f042f4bd8475586c3d55858c6dbb02131b75f1c'
