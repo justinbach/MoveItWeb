034b87188d3067f09e57f62dc64d938ae721ae78
Justin Bachorik <justinbach@gmail.com>
2013-06-15 17:03:28 -0400

    devise installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 MoveItWeb::Application.routes.draw do
+
+  devise_for :users
+
   resources :levels
 
 
@@ -51,7 +54,7 @@ MoveItWeb::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'levels#index'
 
   # See how all your routes lay out with "rake routes"
 
